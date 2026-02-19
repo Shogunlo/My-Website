1f82f88b48bcea7011bfa7354c2b8712e120368f
Ben Marshall <shoguntlingit@gmail.com>
2026-02-19 00:20:06 -0800

    still trying to get it to appear on github pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,7 @@
-fetch("/components/nav.html")
+fetch("components/nav.html")
 .then(response => response.text())
 .then(data => {
     document.getElementById("nav-component").innerHTML = data;
-
-    (function() {
-      const repo = location.pathname.split('/').filter(Boolean)[0] || '';
-      const prefix = repo ? '/' + repo + '/' : '/';
-      document.querySelectorAll('#nav-component a').forEach(a => {
-        const h = a.getAttribute('href') || '';
-        const clean = h.replace(/^(\.\.\/)+/, '');
-        a.href = prefix + clean;
-      });
-    })();
 
     const hamburger = document.getElementById("hamburger");
     const navList = document.getElementById("nav-list");
