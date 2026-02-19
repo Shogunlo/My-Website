850d2bc03205fed15af0cb098082b67a3f040aff
Ben Marshall <shoguntlingit@gmail.com>
2026-02-18 17:31:54 -0800

    hopefully fixed the issue with the nav bar not working on git hub as well as added a third page. It should search using the root now instead of getting stuck in all the sub directories

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
-fetch("../components/nav.html")
+const repoBase = location.hostname.includes("github.io")
+  ? "/My-Website"
+  : "";
+
+fetch(`${repoBase}/components/nav.html`)
 .then(response => response.text())
 .then(data => {
         document.getElementById("nav-component").innerHTML = data;
