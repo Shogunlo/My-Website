c28a3af3a7bf574073c497163742af2b69d6e1cc
Ben Marshall <shoguntlingit@gmail.com>
2026-03-10 16:33:17 -0700

    added a bunch of fixes for breakpoints. changed out some images on Why? page. Added google font and font awesome icons to the navbar and they are being injected via JS

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,11 @@ fetch("components/nav.html")
 .then(response => response.text())
 .then(data => {
     document.getElementById("nav-component").innerHTML = data;
+//should add font awesome here so it loads before the nav icons are used
+    const fa = document.createElement('link');
+    fa.rel = 'stylesheet';
+    fa.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
+    document.head.appendChild(fa);
 
     const hamburger = document.getElementById("hamburger");
     const navList = document.getElementById("nav-list");
